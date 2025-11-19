29a896a86189e01cb74f0223ad74e412b2361f3e
Aurelien <winsaurelien@gmail.com>
2025-11-19 11:25:32 +0100

    Add variable.py

--- variable.py
+++ variable.py
@@ -1,15 +1,14 @@
-# recolter une valeur porte monnaie
-wallet = int(input("Entrer le nombre d'€ que vous possedez"))
+# recolter une valeur du porte monnaie
+wallet = int(input("Entrer le nombre d'argent que vous avez"))
 print("Vous avez actuellement", wallet, "euros")
  
-# creer un produit qui aura pour valeur 50
-produit = 50
+# creer un produit qui aura pour valeur 60 euros
+produit = 60
 print("Le Produit vaut ", produit, "euros")
  
 # enleve au "wallet" le prix du produit
 wallet = wallet - produit
-# ou wallet -= produit
- 
-# afficher la nouvelle valeur
-print("Produit acheté !")
+
+# afficher la nouvelle valeur du porte monnaie
+print("Le produit a bien été acheté ")
 print("Il ne vous reste plus que", wallet, "euros")
